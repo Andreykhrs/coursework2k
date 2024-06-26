2fa831746a4ac86e5a7be386714c8c9ef46ca1d0
Andreykhrs <andrey_khrs@mail.ru>
2024-06-27 01:07:09 +0800

    Initial commit!

---
+++ src/main/java/pro/sky/course_work2k/CourseWork2kApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.course_work2kurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork2kursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWork2kursApplication.class, args);
+	}
+
+}
